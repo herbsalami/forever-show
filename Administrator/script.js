8d2b1eec16663a59576e48b8350faf9f09685f8d
Paris Leach <parisleach@Pariss-MacBook-Pro.local>
2018-05-14 19:38:55 -0400

    started building out architecture for file upload

--- Administrator/script.js
+++ Administrator/script.js
@@ -1,8 +1,17 @@
+let mixes = [];
 if(!sessionStorage.getItem('foreverShowBearerToken')) {
 	document.getElementById('main').innerHTML = '<h1> PLEASE LOGIN <a href="/login">HERE</a> TO VIEW THIS PAGE </h1>';
 }
 else {
+
+	const showSpinner = () => {
+		document.getElementById('mask').style.display = 'flex';
+	}
+	const hideSpinner = () => {
+		document.getElementById('mask').style.display = 'none';
+	}
 	const saveListOrder = () => {
+		showSpinner();
 		let content = Array.from(document.getElementsByClassName('list-group-item')).map((item, index) => {
 				return {
 					id: item.getAttribute('data-value'),
@@ -19,41 +28,59 @@ else {
 		}).then(response => response.json())
 		.then((data) => {
 			console.dir(data);
+			hideSpinner();
 		})
 	};
-	let mixes = [];
-	let list = document.createElement('ul');
-	list.className = 'list-group';
-	list.id = 'simpleList';
-	fetch(`/admin/db/mixes?token=${sessionStorage.getItem('foreverShowBearerToken')}`)
-	.then(response => response.json())
-	.then((data) => {
-		data.results.sort((a, b) => {
-			if(a['index'] < b['index']) {
-				return -1;
-			}
-			else {
-				return 1;
-			}
-		}).forEach((item, index) => {
-			mixes.push(item);
-			let mix = document.createElement('li');
-			mix.setAttribute('data-value', item._id);
-			mix.className = 'list-group-item';
-			mix.innerHTML = `<h7>${item.name}</h7> <div><h9>${item.artist}</h9></div>`;
-			list.appendChild(mix);
-			console.dir(mix);
-		})
-		document.getElementById('schedule').appendChild(list);
-		console.dir(mixes);
-		let sortable = Sortable.create(simpleList);
-		let button = document.createElement('button');
-		button.id = 'print';
-		button.innerHTML = 'Save Schedule Order';
-		document.getElementById('main').appendChild(button);
-		document.getElementById('print').addEventListener('click', saveListOrder);
-	})
 
+	const deleteChildren = () => {
+		let list = document.getElementById('simpleList')
+		while (list.firstChild) {
+	    		list.removeChild(list.firstChild);
+			}
+		return list;
+	}
+
+	const createList = () => {
+		let list = document.createElement('ul');
+		list.className = 'list-group';
+		list.id = 'simpleList';
+		return list;
+	}
+
+	const getMixes = () => {
+		showSpinner();
+		let list = document.getElementById('simpleList') ? deleteChildren() : createList();
+		fetch(`/admin/db/mixes?token=${sessionStorage.getItem('foreverShowBearerToken')}`)
+		.then(response => response.json())
+		.then((data) => {
+			data.results.sort((a, b) => {
+				if(a['index'] < b['index']) {
+					return -1;
+				}
+				else {
+					return 1;
+				}
+			}).forEach((item, index) => {
+				mixes.push(item);
+				let mix = document.createElement('li');
+				mix.setAttribute('data-value', item._id);
+				mix.className = 'list-group-item';
+				mix.innerHTML = `<h7>${item.name}</h7> <div><h9>${item.artist}</h9></div>`;
+				list.appendChild(mix);
+				console.dir(mix);
+			})
+			document.getElementById('schedule').appendChild(list);
+			console.dir(mixes);
+			let sortable = Sortable.create(simpleList);
+			hideSpinner();
+		})
+	}
+	getMixes();
+	let button = document.createElement('button');
+	button.id = 'print';
+	button.innerHTML = 'Save Schedule Order';
+	document.getElementById('schedule-section').appendChild(button);
+	document.getElementById('print').addEventListener('click', saveListOrder);
 
 }
 
--- Routes/db.js
+++ Routes/db.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const url = process.env.DB_URL;
+const fileUpload = require('express-fileupload');
+router.use(fileUpload());
 
 router.use(express.json({ extended: true }));
 
@@ -41,12 +43,10 @@ const updateOrder = (req, res, next) => {
 			db.updateOne({_id: (new mongo.ObjectID(item.id))}, {$set: {index: item.index}}, (err, result) => {
 				console.dir(result);
 			})
-		}, () => {
-			client.close();
-			next();
 		});
 
-
+		client.close();
+		next();
 		// db.find({}, (err, items) => {
 		// 	console.dir(items);
 		// 	res.results = items;
@@ -56,6 +56,23 @@ const updateOrder = (req, res, next) => {
 	});
 };
 
+const createMix = (req, res, next) => {
+
+	if (!req.files)
+    	return res.status(400).send('No files were uploaded.');
+	let newMix = req.files.newMix;
+ 
+  // Use the mv() method to place the file somewhere on your server
+  	newMix.mv(`../Mixes/${newMix.name}`, (err) => {
+    if (err)
+      return res.status(500).send(err);
+    else {
+    	next();
+    }
+  });
+
+}
+
 router.get('/mixes', getMixes, (req, res) => {
 	console.dir(res.results);
 	res.json({results: res.results});
@@ -63,6 +80,10 @@ router.get('/mixes', getMixes, (req, res) => {
 
 router.put('/mixes', updateOrder, (req, res) => {
 	res.json({message: "Well done"});
+})
+
+router.post('/mixes', createMix, (req, res) => {
+	res.json({message: "Mix Created"});
 })
 
 module.exports = router;
